45753478b617ad1740ddecd4f779583ffe8817aa
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-05-16 13:01:56 +0900

    Add: app.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,13 @@
-import classNames from './App.module.css';
+import classNames from "./App.module.css";
+import { TopPage } from "./components/TopPage/index";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const App = () => {
-  return <h1 className={classNames.heading}>Hello React!</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TopPage />
+    </QueryClientProvider>
+  );
 };
