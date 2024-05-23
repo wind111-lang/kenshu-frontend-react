645c36ee7443fd13bd4295934a7e1738953f97de
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-05-23 11:30:18 +0900

    Fix some naming

--- app/src/components/TopPage/Tasklist.tsx
+++ app/src/components/TopPage/Tasklist.tsx
@@ -1,61 +1,60 @@
-import { updateTask } from "../../api/updateTask";
+import { useUpdateTask } from "../../api/useUpdateTask";
 import { useQueryTask } from "../../api/useQueryTask";
-import { useState } from 'react';
+import { useState } from "react";
 
 type Props = {
-    id: string;
-    title: string;
-    createdAt: string;
-    finishedAt: string | null;
+  id: string;
+  title: string;
+  createdAt: string;
+  finishedAt: string | null;
 };
 
+export const TaskList = ({ id, title, createdAt, finishedAt }: Props) => {
+  const queryTask = useQueryTask();
+  const { mutateAsync } = useUpdateTask();
 
-export const Tasklist = ({id, title, createdAt, finishedAt}: Props) => {
-    const querytask = useQueryTask();
-    const updateMutate = updateTask();
+  const [isEditing, setIsEditing] = useState(false);
+  const onClickTitle = () => setIsEditing(true);
 
-    const [isEditing, setIsEditing] = useState(false);
-    const onClickTitle = () => setIsEditing(true);
+  const [titleValue, setTitleValue] = useState(title);
+  const onChange = ({ target: { value } }) => setTitleValue(value);
 
-    const [titleValue, setTitleValue] = useState(title);
-    const onChange = (({ target: { value } }) => 
-        setTitleValue(value));
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-    const onSubmit = (async (e) => {
-        e.preventDefault();
+    await mutateAsync({
+      id,
+      title: titleValue,
+    });
+    setIsEditing(false);
+    void queryTask.refetch();
+  };
 
-        await updateMutate.mutateAsync({
-            id,
-            title: titleValue,
-        });
-        setIsEditing(false);
-        void querytask.refetch();
-    })
-
-    return (
-        <>
-          <div>
-            <p>ID: {id}</p>
-            <p>タイトル: {title}</p>
-            <p>作成日: {createdAt}</p>
-            <p>完了日: {finishedAt}</p>
-          </div>
-            {isEditing ? (
-                <form onSubmit={onSubmit}>
-                <input
-                    className="bg-gray-300"
-                    type='text'
-                    value={titleValue}
-                    onChange={onChange}
-                />
-                </form>
-            ) : (
-                <button className = "bg-yellow-500 border-gray-800"
-                onClick={onClickTitle}
-                >
-                タスク名を編集
-                </button>
-            )}
-        </>
-    );
-}
+  return (
+    <>
+      <div>
+        <p>ID: {id}</p>
+        <p>タイトル: {title}</p>
+        <p>作成日: {createdAt}</p>
+        <p>完了日: {finishedAt}</p>
+      </div>
+      {isEditing ? (
+        <form onSubmit={onSubmit}>
+          <input
+            className="bg-gray-300"
+            type="text"
+            value={titleValue}
+            onChange={onChange}
+          />
+        </form>
+      ) : (
+        <button
+          className="bg-yellow-500 border-gray-800"
+          onClick={onClickTitle}
+        >
+          タスク名を編集
+        </button>
+      )}
+    </>
+  );
+};
--- app/src/components/TopPage/index.tsx
+++ app/src/components/TopPage/index.tsx
@@ -1,60 +1,61 @@
 import { useQueryTask } from "../../api/useQueryTask";
-import { postTask } from "../../api/postTask";
-import { doneTask } from "../../api/doneTask";
-import { resumeTask } from "../../api/resumeTask";
-import { deleteTask } from '../../api/deleteTask';
-import { Tasklist } from "./Tasklist";
+import { usePostTask } from "../../api/usePostTask";
+import { useDoneTask } from "../../api/useDoneTask";
+import { useResumeTask } from "../../api/useResumeTask";
+import { useDeleteTask } from "../../api/useDeleteTask";
+import { TaskList } from "./TaskList";
 
 export const TopPage = () => {
-  const querytask = useQueryTask();
-  const doneTaskMutate = doneTask();
-  const resumeTaskMutate = resumeTask();
-  const deleteTaskMutate = deleteTask();
+  const queryTask = useQueryTask();
+  const hookDoneTaskMutate = useDoneTask();
+  const hookResumeTaskMutate = useResumeTask();
+  const hookDeleteTaskMutate = useDeleteTask();
 
   function formSubmit() {
-    postTask();
-    querytask.refetch();
+    usePostTask();
+    queryTask.refetch();
   }
 
   const finishTask = async (id: string) => {
-    doneTaskMutate.mutate(id)
+    hookDoneTaskMutate.mutate(id);
   };
   const unfinishTask = async (id: string) => {
-    resumeTaskMutate.mutate(id)
-  }; 
-  const deletingTask = async (id: string) => {
-    if (!confirm('削除しますか？')) return;
-    deleteTaskMutate.mutate(id)
+    hookResumeTaskMutate.mutate(id);
   };
-  
+  const deletingTask = async (id: string) => {
+    if (!confirm("削除しますか？")) return;
+    hookDeleteTaskMutate.mutate(id);
+  };
+
   return (
     <div>
       <ul>
-        {querytask.data.tasks.map(({ id, title, createdAt, finishedAt }) => (
+        {queryTask.data.tasks.map(({ id, title, createdAt, finishedAt }) => (
           <li key={id}>
-            <Tasklist id={id} title={title} createdAt={createdAt} finishedAt={finishedAt} />
-            {finishedAt != null ?  (
-              <button 
+            <TaskList
+              id={id}
+              title={title}
+              createdAt={createdAt}
+              finishedAt={finishedAt}
+            />
+            {finishedAt != null ? (
+              <button
                 className="bg-blue-700 text-white"
                 onClick={() => unfinishTask(id)}
               >
                 未完了
               </button>
             ) : (
-              <button 
-                className="bg-green-400"
-                onClick={() => finishTask(id)}
-              >
-              完了
+              <button className="bg-green-400" onClick={() => finishTask(id)}>
+                完了
               </button>
-            )
-           }
-           <button
-             className="bg-red-700 text-white"
-             onClick={() => deletingTask(id)}
-             >
+            )}
+            <button
+              className="bg-red-700 text-white"
+              onClick={() => deletingTask(id)}
+            >
               削除
-           </button>
+            </button>
           </li>
         ))}
       </ul>
